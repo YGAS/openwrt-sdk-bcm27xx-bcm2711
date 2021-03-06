cdb2467fcf433eaf4f2671c7b89b3ee9a82f1f22
YGAS <ycl310397039@hotmail.com>
2021-03-06 21:44:00 +0800

    add sdk

---
+++ build_dir/target-aarch64_cortex-a72_musl/linux-bcm27xx_bcm2711/linux-5.4.102/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP Fri Mar 5 13:56:50 2021"
+#define LINUX_COMPILE_BY "builder"
+#define LINUX_COMPILE_HOST "buildhost"
+#define LINUX_COMPILER "gcc version 8.4.0 (OpenWrt GCC 8.4.0 r16106-8d24da1470)"
