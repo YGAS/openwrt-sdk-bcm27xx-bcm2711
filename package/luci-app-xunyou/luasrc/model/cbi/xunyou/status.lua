914e5060c5d42e60babb8b5fed1b0cafd860489c
YGAS <ycl310397039@hotmail.com>
2021-09-19 11:43:16 +0800

    add xunyou

--- package/luci-app-xunyou/luasrc/model/cbi/xunyou/status.lua
+++ package/luci-app-xunyou/luasrc/model/cbi/xunyou/status.lua
@@ -2,10 +2,6 @@ local m, s
 local uci = luci.model.uci.cursor()
 local sys = require 'luci.sys'
 
-m = Map("xunyou", translate("迅游加速器"), translate("迅游是一款小巧且功能强大的网游加速器，下载仅需10秒左右，一键快捷安装，全面兼容win10在内的所有windows系统。迅游所采用的第五代网游加速技术能更有效地为您解决网游卡机、掉线、延时高、登录难等问题")
-.. translatef("For further information "
-.. "<a href=\"%s\" target=\"_blank\">"
-.. "访问官网</a>", "https://www.xunyou.com/"))
 
 -- s = m:section(TypedSection, 'MySection', translate('基本设置'))
 -- s.anonymous = true
@@ -13,7 +9,12 @@ m = Map("xunyou", translate("迅游加速器"), translate("迅游是一款小巧
 -- o.rawhtml = true
 -- o.version = sys.exec('uci get jd-dailybonus.@global[0].version')
 -- o.template = 'xunyou/service'
-
+m = SimpleForm("xunyou", translate("迅游"), translate("迅游是一款小巧且功能强大的网游加速器，下载仅需10秒左右，一键快捷安装，全面兼容win10在内的所有windows系统。迅游所采用的第五代网游加速技术能更有效地为您解决网游卡机、掉线、延时高、登录难等问题")
+.. translatef("For further information "
+.. "<a href=\"%s\" target=\"_blank\">"
+.. "访问官网</a>", "https://www.xunyou.com/"))
+m.submit=false
+m.reset=false
 m:section(SimpleSection).template  = "xunyou/xunyou"
 
 -- s=m:section(TypedSection, "linkease", translate("Global settings"))
